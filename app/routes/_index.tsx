0a8e7840b6e0c7c911bd64d83b8391c6523cfd53
franmarram67 <pakinro7rai@gmail.com>
2026-09-01 17:32:21 +0200

    ☢️ feat: finish todolist1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/_index";
 import { useState } from "react";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "TodoList1" },
     { name: "description", content: "Welcome to TodoList1!" },
@@ -21,11 +21,11 @@ export default function Home() {
   const [buttonPressed, setButtonPressed] = useState(false);
   const [tasks, setTasks] = useState<Array<Task>>([]);
 
-  function addTaskName() {
+  function addTask() {
     setTasks([
       ...tasks,
-      { 
-        id: nextId++, 
+      {
+        id: nextId++,
         name: taskName,
         completed: false
       }
@@ -33,67 +33,123 @@ export default function Home() {
     setTaskName("");
   }
 
-  const addTaskNameKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+  function removeTask(taskId: number) {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  }
+
+  function completeTask(taskId: number) {
+    setTasks(tasks.map(task => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          completed: true
+        }
+      }
+      return task;
+    }));
+  }
+
+  function incompleteTask(taskId: number) {
+    setTasks(tasks.map(task => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          completed: false
+        }
+      }
+      return task;
+    }));
+  }
+
+  const addTaskKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (taskName && taskName.length !== 0 && event.key === 'Enter') {
       setButtonPressed(true);
     }
   }
 
-  const addTaskNameKeyUp = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+  const addTaskKeyUp = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (taskName && taskName.length !== 0 && event.key === 'Enter') {
       setButtonPressed(false);
-      addTaskName();
+      addTask();
     }
   }
 
-  let addTaskNameButton;
+  let addTaskButton;
   if (!taskName || taskName.length === 0) {
-    addTaskNameButton = <button className="border border-gray-400 rounded-sm bg-gray-400 px-4 py-2 hover:cursor-not-allowed " type="button">Add task</button>
+    addTaskButton = <button className="border border-gray-400 rounded-sm bg-gray-400 px-4 py-2 hover:cursor-not-allowed " type="button">Add task</button>
   } else {
     let buttonClassBase = "border rounded-sm px-4 py-2 ";
     let buttonColor = buttonPressed ? "border-red-600 bg-red-600" : "border-red-400 bg-red-400 hover:cursor-pointer hover:bg-red-500 hover:border-red-500 active:border-red-600 active:bg-red-600";
     let buttonClass = buttonClassBase + buttonColor;
-    addTaskNameButton = <button 
-      onClick={addTaskName}
+    addTaskButton = <button
+      onClick={addTask}
       className={buttonClass}
       type="button"
-      >Add task</button>
-    }
-    
-    return <div className="px-8 py-4">
+    >Add task</button>
+  }
+
+  return <div className="px-8 py-4">
     <h1 className="font-bold text-2xl mb-4">TodoList1</h1>
     <main>
       <div className="mb-4">
-        <input 
-          className="border border-zinc-900 rounded-sm px-4 py-2 mr-4 focus:outline-solid focus:outline-1" 
-          type="text" 
-          onKeyDown={addTaskNameKeyDown}
-          onKeyUp={addTaskNameKeyUp}
+        <input
+          className="border border-zinc-900 rounded-sm px-4 py-2 mr-4 focus:outline-solid focus:outline-1"
+          type="text"
+          onKeyDown={addTaskKeyDown}
+          onKeyUp={addTaskKeyUp}
           placeholder="Task name..."
           onChange={e => setTaskName(e.target.value)}
           value={taskName}
         />
-        {addTaskNameButton}
+        {addTaskButton}
       </div>
       <div>
-        {tasks.map(task => (
-          <div key={task.id} className="flex flex-row justify-start items-center mb-2">
+        {tasks.map(task => {
+          let taskButtons;
+          let taskTextClass;
+          if (!task.completed) {
+            taskButtons = <>
+              <button
+                className="border rounded-sm px-4 py-2 border-red-400 bg-red-400 hover:cursor-pointer hover:bg-red-500 hover:border-red-500 active:border-red-600 active:bg-red-600 mr-2"
+                type="button"
+                onClick={() => removeTask(task.id)}
+              >
+                Remove
+              </button>
+              <button
+                className="border rounded-sm px-4 py-2 border-green-400 bg-green-400 hover:cursor-pointer hover:bg-green-500 hover:border-green-500 active:border-green-600 active:bg-green-600"
+                type="button"
+                onClick={() => completeTask(task.id)}
+              >
+                Complete
+              </button>
+            </>;
+            taskTextClass = "mr-3";
+          } else {
+            taskButtons = <>
+              <button
+                className="border rounded-sm px-4 py-2 border-red-400 bg-red-400 hover:cursor-pointer hover:bg-red-500 hover:border-red-500 active:border-red-600 active:bg-red-600 mr-2"
+                type="button"
+                onClick={() => removeTask(task.id)}
+              >
+                Remove
+              </button>
+              <button
+                className="border rounded-sm px-4 py-2 border-amber-400 bg-amber-400 hover:cursor-pointer hover:bg-amber-500 hover:border-amber-500 active:border-amber-600 active:bg-amber-600"
+                type="button"
+                onClick={() => incompleteTask(task.id)}
+              >
+                Incomplete
+              </button>
+            </>;
+            taskTextClass = "mr-3 line-through";
+          }
+          return <div key={task.id} className="flex flex-row justify-start items-center mb-2">
             <div className="bg-red-500 rounded-3xl w-3 h-3 mr-3"></div>
-            <p className="mr-3">{task.name}</p>
-            <button
-              className="border rounded-sm px-4 py-2 border-red-400 bg-red-400 hover:cursor-pointer hover:bg-red-500 hover:border-red-500 active:border-red-600 active:bg-red-600 mr-2"
-              type="button"
-            >
-              Remove
-            </button>
-            <button
-              className="border rounded-sm px-4 py-2 border-green-400 bg-green-400 hover:cursor-pointer hover:bg-green-500 hover:border-green-500 active:border-green-600 active:bg-green-600"
-              type="button"
-            >
-              Complete
-            </button>
+            <p className={taskTextClass}>{task.name}</p>
+            {taskButtons}
           </div>
-        ))}
+        })}
       </div>
     </main>
   </div>;
